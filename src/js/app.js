b7e42f1b24ed7c1393cb33f67f087bd249ce7227
Jainam C Shah <jainamcshah98@gmail.com>
2019-01-10 01:43:06 +0530

    Create dynamic payment details modal

--- src/js/app.js
+++ src/js/app.js
@@ -5,29 +5,29 @@ const DISCOVERY_DOCS = [
 ];
 const SCOPES = 'https://www.googleapis.com/auth/youtube.readonly';
 
-const signInPage = document.getElementById('sign-in-page');
-const homePage = document.getElementById('home-page');
+const signInPage = $('#sign-in-page')[0];
+const homePage = $('#home-page')[0];
 
-const authorizeButton = document.getElementById('authorize-button');
-const signoutButton = document.getElementById('signout-button');
-const content = document.getElementById('content');
-const registrationForm = document.getElementById('registration-form');
-const channelForm = document.getElementById('channel-form');
-const channelInput = document.getElementById('channel-input');
-const videoContainer = document.getElementById('video-container');
+const authorizeButton = $('#authorize-button')[0];
+const signoutButton = $('#signout-button')[0];
+const content = $('#content')[0];
+const registrationForm = $('#registration-form')[0];
+const channelForm = $('#channel-form')[0];
+const channelInput = $('#channel-input')[0];
+const videoContainer = $('#video-container')[0];
 
 const defaultChannel = 'thenewboston';
 
 // Registration form submit
 registrationForm.addEventListener('submit', e => {
   e.preventDefault();
-  const channelName = document.getElementById('channel-name-input').value;
-  const contact = document.getElementById('contact-input').value;
-  const feeRate = document.getElementById('fee-rate-input').value;
-  const maxViews = document.getElementById('max-views-input').value;
+  const channelName = $('#channel-name-input').val();
+  const contact = $('#contact-input').val();
+  const feeRate = $('#fee-rate-input').val();
+  const maxViews = $('#max-views-input').val();
 
-  if(channelName == "" || contact == "" || feeRate < 1 || maxViews < 1){
-    alert("Please enter all fields");
+  if (channelName == "" || contact == "" || feeRate < 1 || maxViews < 1) {
+    M.toast({ html: 'Please enter all fields!' })
     return;
   }
   console.log(channelName, contact, feeRate, maxViews);
@@ -42,11 +42,38 @@ channelForm.addEventListener('submit', e => {
   getChannel(channel);
 });
 
+
+// Payment Modal click handlers
+
+$(document).on('click', '.sponsor-btn', function (e) {
+  const id = e.target.id;
+  const title = e.target.title;
+  const views = e.target.parentElement.childNodes[3].innerHTML;
+  const iframe = e.target.parentElement.childNodes[1].outerHTML
+  console.log(id, title, views, iframe);
+
+  const data = `
+    <h5>${title}</h5>
+    <h4>${views}</h4>
+    <div style="width: 50%">${iframe}</div>
+    <h5>Rate: 10 wei / view</h5>
+    <h5>Max: 1000 views / week</h5>
+  `;
+
+  $('#modal-left-content').html(data);
+});
+
+$(document).on('click', '#pay-btn', function (e) {
+  console.log("hey", $('#email-message').val());
+});
+
+
 // Load auth2 library
 function handleClientLoad() {
   import('./credentials.mjs').then(module => {
     CLIENT_ID = module.CLIENT_ID;
   });
+  $('.modal').modal();
   gapi.load('client:auth2', initClient);
 }
 
@@ -93,7 +120,7 @@ function handleSignoutClick() {
 
 // Display channel data
 function showChannelData(data) {
-  const channelData = document.getElementById('channel-data');
+  const channelData = $('#channel-data')[0];
   channelData.innerHTML = data;
 }
 
@@ -111,7 +138,7 @@ function getChannel(channel) {
       const output = `
           <span>Title: <span class="channel-details"> ${channel.snippet.title}</span></span>
           <a target="_blank" href="https://youtube.com/${
-          channel.snippet.customUrl
+        channel.snippet.customUrl
         }">(Visit Channel)</a>
           <br>
           <span>Subscribers: <span class="channel-details"> ${numberWithCommas(
@@ -129,7 +156,7 @@ function getChannel(channel) {
       const playlistId = channel.contentDetails.relatedPlaylists.uploads;
       requestVideoPlaylist(playlistId);
     })
-    .catch(err => alert('No Channel By That Name'));
+    .catch(err => M.toast({ html: 'No channel by that name' }));
 }
 
 // Add commas to number
@@ -139,11 +166,11 @@ function numberWithCommas(x) {
 
 // Display videos data
 function showVideoData(data) {
-  const videos = document.getElementById('video-container');
+  const videos = $('#video-container')[0];
   videos.innerHTML += data;
 }
 
-function getVideoData(videoId){
+function getVideoData(videoId, title) {
   const options = {
     id: videoId,
     part: 'statistics'
@@ -159,7 +186,7 @@ function getVideoData(videoId){
       <div class="col s4 video-col">
       <iframe width="100%" height="auto" src="https://www.youtube.com/embed/${videoId}" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
       <span class="views">views: ${numberWithCommas(viewsCount)}</span>
-      <a class="red waves-effect waves-light btn-small right">Sponsor</a>
+      <a id="${videoId}" title="${title}" class="sponsor-btn modal-trigger red waves-effect waves-light btn-small right" href="#payment-modal">Sponsor</a>
       </div>
       `;
     showVideoData(videoOutput);
@@ -185,7 +212,8 @@ function requestVideoPlaylist(playlistId) {
       // Loop through videos and append output
       playListItems.forEach(item => {
         const videoId = item.snippet.resourceId.videoId;
-        getVideoData(videoId);        
+        const title = item.snippet.title;
+        getVideoData(videoId, title);
 
       });
 
