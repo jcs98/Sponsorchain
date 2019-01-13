a47827ada32398b8f2e540b8abb09d8d4f4b6361
Jainam C Shah <jainamcshah98@gmail.com>
2019-01-13 21:57:09 +0530

    Integrate with web3

--- src/js/app.js
+++ src/js/app.js
@@ -1,5 +1,4 @@
 // Options
-let CLIENT_ID;
 const DISCOVERY_DOCS = [
   'https://www.googleapis.com/discovery/v1/apis/youtube/v3/rest'
 ];
@@ -7,52 +6,121 @@ const SCOPES = 'https://www.googleapis.com/auth/youtube.readonly';
 
 const signInPage = $('#sign-in-page')[0];
 const homePage = $('#home-page')[0];
-
-const authorizeButton = $('#authorize-button')[0];
-const signoutButton = $('#signout-button')[0];
-const content = $('#content')[0];
-const registrationForm = $('#registration-form')[0];
-const channelForm = $('#channel-form')[0];
-const channelInput = $('#channel-input')[0];
-const videoContainer = $('#video-container')[0];
-
 const defaultChannel = 'thenewboston';
 
-// Registration form submit
-registrationForm.addEventListener('submit', e => {
-  e.preventDefault();
-  const channelName = $('#channel-name-input').val();
-  const contact = $('#contact-input').val();
-  const feeRate = $('#fee-rate-input').val();
-  const maxViews = $('#max-views-input').val();
+App = {
+  web3Provider: null,
+  contracts: {},
 
-  if (channelName == "" || contact == "" || feeRate < 1 || maxViews < 1) {
-    M.toast({ html: 'Please enter all fields!' })
-    return;
-  }
-  console.log(channelName, contact, feeRate, maxViews);
-  // registerNewCreator(channelName, contact, feeRate, maxViews);
-  // getMyChannel();
-});
+  init: async function () {
+    // Load data
+    return await App.initWeb3();
+  },
 
-// Form submit and change channel
-channelForm.addEventListener('submit', e => {
-  e.preventDefault();
-  const channel = channelInput.value;
-  getChannel(channel);
-});
+  initWeb3: async function () {
+    // Modern dapp browsers...
+    if (window.ethereum) {
+      App.web3Provider = window.ethereum;
+      try {
+        // Request account access
+        await window.ethereum.enable();
+      } catch (error) {
+        // User denied account access...
+        console.error("User denied account access")
+      }
+    }
+    // Legacy dapp browsers...
+    else if (window.web3) {
+      App.web3Provider = window.web3.currentProvider;
+    }
+    // If no injected web3 instance is detected, fall back to Ganache
+    else {
+      App.web3Provider = new Web3.providers.HttpProvider('http://localhost:7545');
+    }
+    web3 = new Web3(App.web3Provider);
+
+    return App.initContract();
+  },
+
+  initContract: function () {
+    // $.getJSON('Sponsor.json', function (data) {
+    //   var SponsorArftifact = data;
+    //   App.contracts.Sponsor = TruffleContract(SponsorArftifact);
+    //   App.contracts.Sponsor.setProvider(App.web3Provider);
+    //   return App.markAdopted();
+    // });
+
+    return App.bindEvents();
+  },
+
+  bindEvents: function () {
+    $('#authorize-button').click(App.handleAuthClick);
+    $('#signout-button').click(App.handleSignoutClick);
+
+    $(document).on('click', '.sponsor-btn', App.handleSponsorBtnClick);
+    $('#pay-btn').click(App.handlePayBtnClick);
+    $('#update-btn').click(App.handleUpdateBtnClick);
+
+    $('#registration-form').submit(App.handleRegistrationFormSubmit);
+    $('#channel-form').submit(App.handleChannelFormSubmit);
+  },
+
+  // Load auth2 library
+  handleClientLoad: function () {
+    import('./credentials.mjs').then(module => {
+      CLIENT_ID = module.CLIENT_ID;
+    });
+    $('.modal').modal();
+    gapi.load('client:auth2', App.initClient);
+  },
+
+  // Init API client library and set up sign in listeners
+  initClient: function () {
+    gapi.client
+      .init({
+        discoveryDocs: DISCOVERY_DOCS,
+        clientId: CLIENT_ID,
+        scope: SCOPES
+      })
+      .then(() => {
+        // Listen for sign in state changes
+        gapi.auth2.getAuthInstance().isSignedIn.listen(App.updateSigninStatus);
+        // Handle initial sign in state
+        App.updateSigninStatus(gapi.auth2.getAuthInstance().isSignedIn.get());
+      });
+  },
+
+  // Update UI sign if state changes
+  updateSigninStatus: function (isSignedIn) {
+    if (isSignedIn) {
+      homePage.style.display = 'block';
+      signInPage.style.display = 'none';
+      console.log(gapi.client);
+      App.getChannel(defaultChannel);
+    } else {
+      homePage.style.display = 'none';
+      signInPage.style.display = 'block';
+    }
+  },
 
 
-// Payment Modal click handlers
+  // Click handlers
+  handleAuthClick: function () {
+    gapi.auth2.getAuthInstance().signIn();
+  },
 
-$(document).on('click', '.sponsor-btn', function (e) {
-  const id = e.target.id;
-  const title = e.target.title;
-  const views = e.target.parentElement.childNodes[3].innerHTML;
-  const iframe = e.target.parentElement.childNodes[1].outerHTML
-  console.log(id, title, views, iframe);
+  handleSignoutClick: function () {
+    gapi.auth2.getAuthInstance().signOut();
+  },
 
-  const data = `
+  handleSponsorBtnClick: function (e) {
+    const id = e.target.id;
+    const title = e.target.title;
+    const views = e.target.parentElement.childNodes[3].innerHTML;
+    const iframe = e.target.parentElement.childNodes[1].outerHTML
+    console.log(id, title, views, iframe);
+
+    const data = `
     <h5>${title}</h5>
     <h4>${views}</h4>
     <div style="width: 50%">${iframe}</div>
@@ -60,171 +128,137 @@ $(document).on('click', '.sponsor-btn', function (e) {
     <h5>Max: 1000 views / week</h5>
   `;
 
-  $('#modal-left-content').html(data);
-});
+    $('#modal-left-content').html(data);
+  },
 
-$(document).on('click', '#pay-btn', function (e) {
-  console.log("hey", $('#email-message').val());
-});
+  handlePayBtnClick: function (e) {
+    console.log("hey", $('#email-message').val());
+  },
 
-$(document).on('click', '#update-btn', function (e) {
-  console.log("update", $('#fee-rate-update-input').val(), $('#max-views-update-input').val());
-});
+  handleUpdateBtnClick: function (e) {
+    console.log("update", $('#fee-rate-update-input').val(), $('#max-views-update-input').val());
+  },
 
 
-// Load auth2 library
-function handleClientLoad() {
-  import('./credentials.mjs').then(module => {
-    CLIENT_ID = module.CLIENT_ID;
-  });
-  $('.modal').modal();
-  gapi.load('client:auth2', initClient);
-}
+  // Form submit handlers
+  handleRegistrationFormSubmit: function (e) {
+    e.preventDefault();
+    const channelName = $('#channel-name-input').val();
+    const contact = $('#contact-input').val();
+    const feeRate = $('#fee-rate-input').val();
+    const maxViews = $('#max-views-input').val();
 
-// Init API client library and set up sign in listeners
-function initClient() {
-  gapi.client
-    .init({
-      discoveryDocs: DISCOVERY_DOCS,
-      clientId: CLIENT_ID,
-      scope: SCOPES
-    })
-    .then(() => {
-      // Listen for sign in state changes
-      gapi.auth2.getAuthInstance().isSignedIn.listen(updateSigninStatus);
-      // Handle initial sign in state
-      updateSigninStatus(gapi.auth2.getAuthInstance().isSignedIn.get());
-      authorizeButton.onclick = handleAuthClick;
-      signoutButton.onclick = handleSignoutClick;
-    });
-}
+    if (channelName == "" || contact == "" || feeRate < 1 || maxViews < 1) {
+      M.toast({ html: 'Please enter all fields!' })
+      return;
+    }
+    console.log(channelName, contact, feeRate, maxViews);
+    // registerNewCreator(channelName, contact, feeRate, maxViews);
+    // getMyChannel();
+  },
 
-// Update UI sign if state changes
-function updateSigninStatus(isSignedIn) {
-  if (isSignedIn) {
-    homePage.style.display = 'block';
-    signInPage.style.display = 'none';
-    console.log(gapi.client);
-    getChannel(defaultChannel);
-  } else {
-    homePage.style.display = 'none';
-    signInPage.style.display = 'block';
-  }
-}
+  handleChannelFormSubmit: function (e) {
+    e.preventDefault();
+    const channel = $('#channel-input').val();
+    App.getChannel(channel);
+  },
 
-// Handle login
-function handleAuthClick() {
-  gapi.auth2.getAuthInstance().signIn();
-}
 
-// Handle logout
-function handleSignoutClick() {
-  gapi.auth2.getAuthInstance().signOut();
-}
+  // Add commas to number (helper)
+  numberWithCommas: function (x) {
+    return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+  },
 
-// Display channel data
-function showChannelData(data) {
-  const channelData = $('#channel-data')[0];
-  channelData.innerHTML = data;
-}
+  // Get channel from API
+  getChannel: function (channel) {
+    gapi.client.youtube.channels
+      .list({
+        part: 'snippet,contentDetails,statistics',
+        forUsername: channel
+      })
+      .then(response => {
+        console.log(response);
+        const channel = response.result.items[0];
 
-// Get channel from API
-function getChannel(channel) {
-  gapi.client.youtube.channels
-    .list({
-      part: 'snippet,contentDetails,statistics',
-      forUsername: channel
-    })
-    .then(response => {
-      console.log(response);
-      const channel = response.result.items[0];
-
-      const output = `
+        const output = `
           <span>Title: <span class="channel-details"> ${channel.snippet.title}</span></span>
           <a target="_blank" href="https://youtube.com/${
-        channel.snippet.customUrl
-        }">(Visit Channel)</a>
+          channel.snippet.customUrl
+          }">(Visit Channel)</a>
           <br>
-          <span>Subscribers: <span class="channel-details"> ${numberWithCommas(
-          channel.statistics.subscriberCount
-        )}</span></span>
-          <span>Views: <span class="channel-details"> ${numberWithCommas(
-          channel.statistics.viewCount
-        )}</span></span>
-          <span>Videos: <span class="channel-details"> ${numberWithCommas(
-          channel.statistics.videoCount
-        )}</span></span>        
+          <span>Subscribers: <span class="channel-details"> ${App.numberWithCommas(
+            channel.statistics.subscriberCount
+          )}</span></span>
+          <span>Views: <span class="channel-details"> ${App.numberWithCommas(
+            channel.statistics.viewCount
+          )}</span></span>
+          <span>Videos: <span class="channel-details"> ${App.numberWithCommas(
+            channel.statistics.videoCount
+          )}</span></span>        
       `;
-      showChannelData(output);
+        $('#channel-data').html(output);
 
-      const playlistId = channel.contentDetails.relatedPlaylists.uploads;
-      requestVideoPlaylist(playlistId);
-    })
-    .catch(err => M.toast({ html: 'No channel by that name' }));
-}
+        const playlistId = channel.contentDetails.relatedPlaylists.uploads;
+        App.requestVideoPlaylist(playlistId);
+      })
+      .catch(err => M.toast({ html: 'No channel by that name' }));
+  },
 
-// Add commas to number
-function numberWithCommas(x) {
-  return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
-}
+  // Get individual video data
+  getVideoData: function (videoId, title) {
+    const options = {
+      id: videoId,
+      part: 'statistics'
+    }
 
-// Display videos data
-function showVideoData(data) {
-  const videos = $('#video-container')[0];
-  videos.innerHTML += data;
-}
-
-function getVideoData(videoId, title) {
-  const options = {
-    id: videoId,
-    part: 'statistics'
-  }
-
-  const req = gapi.client.youtube.videos.list(options);
-  let viewsCount;
-  req.execute(res => {
-    // console.log(res);
-    viewsCount = res.items[0].statistics.viewCount;
-    // console.log(viewsCount);
-    const videoOutput = `
+    const req = gapi.client.youtube.videos.list(options);
+    let viewsCount;
+    req.execute(res => {
+      // console.log(res);
+      viewsCount = res.items[0].statistics.viewCount;
+      // console.log(viewsCount);
+      const videoOutput = `
       <div class="col s4 video-col">
       <iframe width="100%" height="auto" src="https://www.youtube.com/embed/${videoId}" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
-      <span class="views">views: ${numberWithCommas(viewsCount)}</span>
+      <span class="views">views: ${App.numberWithCommas(viewsCount)}</span>
       <a id="${videoId}" title="${title}" class="sponsor-btn modal-trigger red waves-effect waves-light btn-small right" href="#payment-modal">Sponsor</a>
       </div>
       `;
-    showVideoData(videoOutput);
-  });
-}
+      $('#video-container')[0].innerHTML += videoOutput;
+    });
+  },
 
-function requestVideoPlaylist(playlistId) {
-  const requestOptions = {
-    playlistId: playlistId,
-    part: 'snippet,contentDetails',
-    maxResults: 6
-  };
+  // Get video playlist
+  requestVideoPlaylist: function (playlistId) {
+    const requestOptions = {
+      playlistId: playlistId,
+      part: 'snippet,contentDetails',
+      maxResults: 6
+    };
+    const request = gapi.client.youtube.playlistItems.list(requestOptions);
+    const videoContainer = $('#video-container')[0];
 
-  const request = gapi.client.youtube.playlistItems.list(requestOptions);
-
-  request.then(response => {
-    console.log(response.result.items);
-    const playListItems = response.result.items;
-    if (playListItems) {
+    request.then(response => {
+      console.log(response.result.items);
+      const playListItems = response.result.items;
       videoContainer.innerHTML = '';
-      let output = '<br><h4 class="center-align">Latest Videos</h4>';
+      if (playListItems) {
+        // Loop through videos and append output
+        playListItems.forEach(item => {
+          const videoId = item.snippet.resourceId.videoId;
+          const title = item.snippet.title;
+          App.getVideoData(videoId, title);
+        });
+      } else {
+        videoContainer.innerHTML = 'No Uploaded Videos';
+      }
+    });
+  }
 
-      // Loop through videos and append output
-      playListItems.forEach(item => {
-        const videoId = item.snippet.resourceId.videoId;
-        const title = item.snippet.title;
-        getVideoData(videoId, title);
+};
 
-      });
-
-      // Output videos
-      videoContainer.innerHTML = output + videoContainer.innerHTML;
-    } else {
-      videoContainer.innerHTML = 'No Uploaded Videos';
-    }
+$(function () {
+  $(window).load(function () {
+    App.init();
   });
-}
+});
